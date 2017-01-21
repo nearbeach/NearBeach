35086b0631b40b3b840048a2583a08512a224c42
Luke Christopher Clarke <luke@NearBeach.tk>
2017-01-21 13:31:57 +1100

    modified:   admin.py 	modified:   apps.py 	modified:   urls.py 	modified:   views.py

--- apps.py
+++ apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class NearbeachConfig(AppConfig):
-    name = 'nearbeach'
+class NearBeachConfig(AppConfig):
+    name = 'NearBeach'
