94ca39e96befdf33ceaf6c1cf764be2b2c9fe391
luke <robotichead@gmail.com>
2017-01-28 17:31:09 +1100

    modified:   forms.py

--- forms.py
+++ forms.py
@@ -1,9 +1,17 @@
 from django import forms
+
+#Import from Models
+from .models import organisations
+
+#Import extra
 import datetime
 
 class new_project_form(forms.Form):
+	#SQL needed for form
+	organisations_results = organisations.objects.all()
+	
 	project_name = forms.CharField(max_length = 255)
 	project_description = forms.CharField(widget = forms.Textarea)
-	organisations_id = forms.CharField()
+	organisations_id = forms.ModelChoiceField(label = "Organisation", widget = forms.Select, queryset = organisations_results)
 	project_start_date = forms.DateTimeField(widget = forms.DateTimeInput)
 	project_end_date = forms.DateTimeField(widget = forms.DateTimeInput)
