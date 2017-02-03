a75e4d8a8ea302279cdf3d14aea7202f5e5ebb88
luke <robotichead@gmail.com>
2017-02-03 17:33:55 +1100

    modified:   forms.py 	modified:   templates/NearBeach/new_project.html 	modified:   templates/NearBeach/template.html 	modified:   views.py

--- views.py
+++ views.py
@@ -222,6 +222,15 @@ def new_project(request):
 		#Load the template
 		t = loader.get_template('NearBeach/new_project.html')
 		
+		#Construct the sets
+		set_hours = ['01',	'02',	'03',	'04',	'05',	'06',	'07',	'08',	'09',	'10',	'11',	'12',]
+		set_minutes = ['00',	'01',	'02',	'03',	'04',	'05',	'06',	'07',	'08',	'09',	'10',	'11',	'12',	'13',	'14',	'15',	'16',	'17',	'18',	'19',	'20',	'21',	'22',	'23',	'24',	'25',	'26',	'27',	'28',	'29',	'30',	'31',	'32',	'33',	'34',	'35',	'36',	'37',	'38',	'39',	'40',	'41',	'42',	'43',	'44',	'45',	'46',	'47',	'48',	'49',	'50',	'51',	'52',	'53',	'54',	'55',	'56',	'57',	'58',	'59',]
+		set_meridiems = ['AM','PM',]
+		
+		set_days = ['01',	'02',	'03',	'04',	'05',	'06',	'07',	'08',	'09',	'10',	'11',	'12',	'13',	'14',	'15',	'16',	'17',	'18',	'19',	'20',	'21',	'22',	'23',	'24',	'25',	'26',	'27',	'28',	'29',	'30',	'31',]
+		set_months = ['January',	'February',	'March',	'April',	'May',	'June',	'July',	'August',	'September',	'October',	'November',	'December',]
+		set_years = ['2010',	'2011',	'2012',	'2013',	'2014',	'2015',	'2016',	'2017',	'2018',	'2019',	'2020',	'2021',	'2022',	'2023',	'2024',	'2025',	'2026',	'2027',	'2028',	'2029',	'2030',	'2031',	'2032',	'2033',	'2034',	'2035',	'2036',	'2037',	'2038',	'2039',	'2040',	'2041',	'2042',	'2043',	'2044',	'2045',]
+		
 		#context
 		c = {
 			'groups_results': groups_results,
@@ -229,6 +238,13 @@ def new_project(request):
 			'organisation_results': organisation_results,
 			'organisation_counts': organisation_results.count(),
 			'new_project_form': new_project_form(),
+			
+			'set_hours': set_hours,
+			'set_minutes': set_minutes,
+			'set_meridiems': set_meridiems,
+			'set_days': set_days,
+			'set_months': set_months,
+			'set_years': set_years,			
 		}
 		
 		return HttpResponse(t.render(c, request))
