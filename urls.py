9b7a99fd8cfdd39d3c43fa6557917d06ca7160ce
luke <luke@Luk-Laptop>
2017-06-07 22:27:05 +1000

    modified:   urls.py

--- urls.py
+++ urls.py
@@ -63,8 +63,6 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
