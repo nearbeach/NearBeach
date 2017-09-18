bbb9786493227409efad43e9c5190d2fae367632
luke <robotichead@gmail.com>
2017-09-18 18:04:14 +1000

    new file:   static/NearBeach/javascript/costs_running_total.js 	modified:   static/NearBeach/javascript/project_information.js 	modified:   static/NearBeach/javascript/task_information.js 	modified:   templates/NearBeach/project_information.html 	modified:   templates/NearBeach/task_information.html

--- static/NearBeach/javascript/task_information.js
+++ static/NearBeach/javascript/task_information.js
@@ -46,30 +46,3 @@ function enable_disable_add_cost() {
 	}
 }
 
-function running_total() {
-	/*
-	Method
-	~~~~~~
-	The running total is calculated AFTER ALL data has been created
-	to help save server memory for large datasets.
-
-	1.) Get variables and set running total = 0
-	2.) LOOP
-	3.) Find cost amount and add to running total
-	4.) Edit running total spot
-	5.) Loop
-	6.) Fill out grand total
-	 */
-	var cost_amount = document.getElementsByName("cost_amount");
-	var running_total = document.getElementsByName("running_total");
-	var grand_total = document.getElementById("grand_total");
-	var running_total_amount = 0;
-
-	//The loop
-	for(var i = 1; i <= running_total.length; i++) {
-		running_total_amount = running_total_amount + Number(cost_amount[i].innerHTML);
-		running_total[i-1].innerHTML = running_total_amount;
-	}
-
-	grand_total.innerHTML = running_total_amount;
-}
