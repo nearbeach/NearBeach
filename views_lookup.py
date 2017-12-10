37c084b4b80af8c3acc685e96c330d66d19d17fa
luke <robotichead@gmail.com>
2017-12-11 08:38:14 +1100

    modified:   templates/NearBeach/quote_information_modules/new_line_item.html 	modified:   views_lookup.py

--- views_lookup.py
+++ views_lookup.py
@@ -1,13 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from .models import *
 from django.core import serializers
-from django.http import JsonResponse
+
 
 @login_required(login_url='login')
 def lookup_product(request, product_id):
-    #Get Data
-    product_results = serializers.serialize(
-        'json',
-        products_and_services.objects.filter(product_id=product_id),
-    )
-    return JsonResponse(product_results, safe=False)
+    product_results = products_and_services.objects.filter(product_id=product_id)
+    data = serializers.serialize("json", product_results)
+    return HttpResponse(data, content_type='application/json')
