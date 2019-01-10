269f25798470e339fab10a449a06180309587f8f
robotichead <robotichead@gmail.com>
2019-01-10 14:10:33 +1100

    Setting up git so it will work with pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NearBeach",
+    version="0.0.1",
+    author="Luke Christopher Clarke",
+    author_email="luke@nearbeach.org",
+    description="NearBeach - an open source project management tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robotichead/NearBeach",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
--- static/NearBeach/javascript/assign_customer_project_task.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by luke on 2/07/17.
- */
-function update_filter() {
-    alert("update filter function")
-}
