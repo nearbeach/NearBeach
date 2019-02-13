39b248f819adbbb3eca0e2f387c29d41594dc12b
robotichead <robotichead@gmail.com>
2019-02-14 00:05:37 +1100

    Construction of linking for requirements - need to make it for requirement items still

--- NearBeach/views_requirements.py
+++ NearBeach/views_requirements.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.template import  loader
 from django.db.models import Sum, Q, Min
@@ -11,6 +11,7 @@ from .models import *
 from .misc_functions import *
 from .user_permissions import return_user_permission_level
 from .views import permission_denied
+
 
 
 
@@ -137,6 +138,134 @@ def new_requirement_item(request, requirement_id):
     }
 
     return HttpResponse(t.render(c, request))
+
+
+
+@login_required(login_url='login')
+def new_requirement_link(request,requirement_id,location_id="",destination=""):
+    """
+    This function is designed so users can link requirement to either tasks/projects. When the function is a simple "GET"
+    function it will produce an HTML page with all the possible object links.
+    When the method is a "POST", it will create the link and return a blank page. Note that for a post it requires location
+    and destination.
+    :param request:
+    :param requirement_id: The requirement we are looking to link
+    :param location_id: The location id of the object we are linking to
+    :param destination: The destination object we are linking to
+    :return: List of potential links
+
+    Method
+    ~~~~~~
+    1. Check permissions
+    2. Check method - if POST read instructions there
+    3. Object a list of all objects that meet the current conditions
+        -- Is not deleted
+        -- Users can access those objects
+        -- Are not currently linked
+        -- Object is currently open
+    4. Present the data to the user
+    """
+    permission_results = return_user_permission_level(request, None, 'requirement')
+
+    if permission_results['requirement'] < 2:
+        return HttpResponseRedirect(reverse('permission_denied'))
+
+    if request.method == "POST":
+        """
+        Method
+        ~~~~~~
+        1. Make sure the location_id and destination are not "" - send error
+        2. Create the requirement_link row
+        3. Return the JSON result
+        4. Profit
+        """
+        if location_id == "" or destination == "":
+            return HttpResponseBadRequest("Sorry - those fields were blank")
+
+        requirement_link_submit = requirement_link(
+            change_user=request.user,
+            requirement_id=requirement_id,
+        )
+        if destination == "project":
+            """
+            1. Get the project instance
+            2. Save the project instance against the requirement_link
+            3. Prepare the object_link and object_description
+            """
+            project_instance = project.objects.get(project_id=location_id)
+            requirement_link_submit.project_id = project_instance
+            object_link = "Project - " + str(location_id)
+            object_description = project_instance.project_name
+        elif destination == "task":
+            """
+            1. Get the task instance
+            2. Save the task instance against the requirement_link
+            3. Prepare the object_link and object_description
+            """
+            task_instance = task.objects.get(task_id=location_id)
+            requirement_link_submit.task_id = task_instance
+            object_link = "Task - " + str(location_id)
+            object_description = task_instance.task_short_description
+
+        # Save
+        requirement_link_submit.save()
+
+        # Send back the JSON
+        return JsonResponse({
+            'location_id': location_id,
+            'destination': destination,
+            'object_link': object_link,
+            'object_description': object_description,
+        })
+
+
+    # Get required data
+    project_results = project.objects.filter(
+        is_deleted="FALSE",
+        project_status__in={'New','Open'},
+        project_id__in=object_assignment.objects.filter(
+            is_deleted="FALSE",
+            group_id__in=user_group.objects.filter(
+                is_deleted="FALSE",
+                username_id=request.user.id,
+            ).values('group_id')
+        ).values('project_id')
+    ).exclude(
+        project_id__in=requirement_link.objects.filter(
+            is_deleted="FALSE",
+            project_id__isnull=False,
+            requirement_id=requirement_id,
+        ).values('project_id')
+    )
+
+    task_results = task.objects.filter(
+        is_deleted="FALSE",
+        task_status__in={'New','Open'},
+        task_id__in=object_assignment.objects.filter(
+            is_deleted="FALSE",
+            group_id__in=user_group.objects.filter(
+                is_deleted="FALSE",
+                username_id=request.user.id,
+            ).values('group_id')
+        ).values('task_id')
+    ).exclude(
+        task_id__in=requirement_link.objects.filter(
+            is_deleted="FALSE",
+            task_id__isnull=False,
+            requirement_id=requirement_id,
+        ).values('task_id')
+    )
+
+    # Get Template
+    t = loader.get_template('NearBeach/requirement_information/new_requirement_link.html')
+
+    # Context
+    c = {
+        'project_results': project_results,
+        'task_results': task_results,
+    }
+
+    return HttpResponse(t.render(c,request))
 
 
 @login_required(login_url='login')
@@ -408,231 +537,17 @@ def requirement_item_information(request, requirement_item_id):
 
 
 @login_required(login_url='login')
-def requirement_items_new_link(request, requirement_item_id, location_id= '', destination=''):
-    permission_results = return_user_permission_level(request, None, 'requirement_link')
-
-    if permission_results['requirement_link'] == 0:
-        return HttpResponseRedirect(reverse('permission_denied'))
-
-    if request.method == "POST":
-        #Check to make sure that there exists a location id and project_or_task value
-        if location_id == '' or destination == '':
-            #Well - this is not good for POST
-            return HttpResponseBadRequest("Please note, both location_id and project_or_task will need to be filled out.")
-
-        requirement_item_link_save = requirement_item_link(
-            requirement_item_id=requirement_item_id,
-            change_user=request.user,
-        )
-
-        if destination == "project":
-            project_instance = project.objects.get(project_id=location_id)
-            requirement_item_link_save.project_id = project_instance
-        elif destination == "task":
-            task_instance = task.objects.get(task_id=location_id)
-            requirement_item_link_save.task_id = task_instance
-        elif destination == "organisation":
-            organisation_instance = organisation.objects.get(organisation_id=location_id)
-            requirement_item_link_save.organisation_id = organisation_instance
-        else:
-            return HttpResponseBadRequest("You can only choose: project, task, or organisation")
-
-        requirement_item_link_save.save()
-
-        #Return blank page\
-        # Load template
-        t = loader.get_template('NearBeach/blank.html')
-
-        # context
-        c = {
-        }
-
-        return HttpResponse(t.render(c, request))
-
+def requirement_readonly(request,requirement_id):
     """
-    The linked items will only link to items the user has access to. Nothing else.
+    Requirement readonly is a read only module. This will print out all the requirement information that the user will
+    require.
+    :param request:
+    :param requirement_id: The requirement that the end user wants to look at.
+    :return: A read only page for the user
     """
-    project_results = project.objects.filter(
-        Q(
-            is_deleted="FALSE",
-            project_id__in=object_assignment.objects.filter(
-                is_deleted="FALSE",
-                group_id__in=user_group.objects.filter(
-                    is_deleted="FALSE",
-                    username=request.user,
-                ).values('group_id').distinct(),
-            ).values('project_id').distinct(),
-        ) and
-        Q(
-            Q(project_status='New') or
-            Q(project_status='Open')
-        )
-    )
-
-    task_results = task.objects.filter(
-        Q(
-            is_deleted="FALSE",
-            task_id__in=object_assignment.objects.filter(
-                is_deleted="FALSE",
-                group_id__in=user_group.objects.filter(
-                    is_deleted="FALSE",
-                    username=request.user,
-                ).values('group_id').distinct(),
-            ).values('task_id').distinct(),
-        ) and
-        Q(
-            Q(task_status='New') or
-            Q(task_status='Open')
-        )
-    )
-
-    #Load template
-    t = loader.get_template('NearBeach/requirement_information/requirement_item_new_link.html')
-
-    # context
-    c = {
-        'requirement_item_id': requirement_item_id,
-        'project_results': project_results,
-        'task_results': task_results,
-    }
-
-    return HttpResponse(t.render(c, request))
-
-
-
-
-
-@login_required(login_url='login')
-def requirement_links_list(request, requirement_id):
     permission_results = return_user_permission_level(request, None, 'requirement')
 
-    if permission_results['requirement'] == 0:
-        return HttpResponseRedirect(reverse('permission_denied'))
-
-    item_results = requirement_item.objects.filter(
-        is_deleted="FALSE",
-        requirement_id=requirement_id,
-    )
-
-    requirement_results = requirement.objects.filter(
-        requirement_id=requirement_id,
-        is_deleted="FALSE",
-    )
-
-
-    #Load template
-    t = loader.get_template('NearBeach/requirement_information/requirement_links_list.html')
-
-    # context
-    c = {
-        'item_results': item_results,
-        'requirement_results': requirement_results,
-    }
-
-    return HttpResponse(t.render(c, request))
-
-
-
-@login_required(login_url='login')
-def requirement_new_link(request, requirement_id, location_id='', destination=''):
-    permission_results = return_user_permission_level(request, None, 'requirement_link')
-
-    if permission_results['requirement_link'] < 2:
-        return HttpResponseRedirect(reverse('permission_denied'))
-
-    if request.method == "POST":
-        print("Requirement ID: " + requirement_id + "\nLocation ID: " + location_id + "\nTask or Project: " + destination)
-        #Check to make sure that there exists a location id and project_or_task value
-        if location_id == '' or destination == '':
-            #Well - this is not good for POST
-            print("Please check the URL")
-            return HttpResponseBadRequest("Please note, both location_id and project_or_task will need to be filled out.")
-
-        print("Trying to save")
-        requirement_link_save = requirement_link(
-            requirement=requirement.objects.get(requirement_id=requirement_id),
-            change_user=request.user,
-        )
-
-        if destination == "project":
-            project_instance = project.objects.get(project_id=location_id)
-            requirement_link_save.project_id = project_instance
-        elif destination == "task":
-            task_instance = task.objects.get(task_id=location_id)
-            requirement_link_save.task_id = task_instance
-        elif destination == "organisation":
-            organisation_instance = organisation.objects.get(organisation_id=location_id)
-            requirement_link_save.organisation_id = organisation_instance
-        else:
-            return HttpResponseBadRequest("You can only choose: project, task, or organisation")
-
-        requirement_link_save.save()
-
-        #Return blank page\
-        # Load template
-        t = loader.get_template('NearBeach/blank.html')
-
-        # context
-        c = {
-        }
-
-        return HttpResponse(t.render(c, request))
-    """
-    The linked requirement will only link to requirement the user has access to. Nothing else.
-    """
-    project_results = project.objects.filter(
-        Q(
-            is_deleted="FALSE",
-            project_id__in=object_assignment.objects.filter(
-                group_id__in=user_group.objects.filter(
-                    username=request.user,
-                    is_deleted="FALSE",
-                ).values('group_id'),
-                is_deleted="FALSE",
-            ).values('project_id').distinct()
-        ) and
-        Q(
-            Q(project_status='New') or
-            Q(project_status='Open')
-        )
-    )
-
-    task_results = task.objects.filter(
-        Q(
-            is_deleted="FALSE",
-            task_id__in=object_assignment.objects.filter(
-                group_id__in=user_group.objects.filter(
-                    username=request.user,
-                    is_deleted="FALSE",
-                ).values('group_id'),
-                is_deleted="FALSE",
-            ).values('task_id').distinct()
-        ) and
-        Q(
-            Q(task_status='New') or
-            Q(task_status='Open')
-        )
-    )
-
-    #Load template
-    t = loader.get_template('NearBeach/requirement_information/requirement_new_link.html')
-
-    # context
-    c = {
-        'requirement_id': requirement_id,
-        'project_results': project_results,
-        'task_results': task_results,
-
-    }
-
-    return HttpResponse(t.render(c, request))
-
-
-@login_required(login_url='login')
-def requirement_readonly(request,requirement_id):
-    permission_results = return_user_permission_level(request, None, 'requirement_link')
-
-    if permission_results['requirement_link'] < 2:
+    if permission_results['requirement_link'] == 0:
         return HttpResponseRedirect(reverse('permission_denied'))
 
     #Get Data
@@ -662,4 +577,4 @@ def requirement_readonly(request,requirement_id):
         'administration_permission': permission_results['administration'],
     }
 
-    return HttpResponse(t.render(c, request))
+    return HttpResponse(t.render(c, request)) 
