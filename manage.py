26cdb39804d49ab891fe829b88ac977b7a65aecc
robotichead <robotichead@gmail.com>
2019-02-18 13:29:46 +1100

    Comments applied in CircleCI files. Can now merge with development branch

--- manage.py
+++ manage.py
@@ -1,3 +1,15 @@
+"""
+This management file is designed for automatic testing purposes. It will not be
+used in;
+- production
+- pypi
+
+This function will only be used in;
+- Automatic testing using CircleCI
+
+This file can also be used - but currently not setup for;
+- Automatic testing using travis
+"""
 import os
 from django.core import management
 
