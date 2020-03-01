f8a008f11b49360c4b4c15f698d0172baee55cbf
Luke <luke@nearbeach.org>
2020-03-01 18:33:06 +1100

    Basic testing with selenium and browser stack

---
+++ tests/browserstack/config_example.py
@@ -0,0 +1,22 @@
+{
+	'browserstack_username': '',
+	'browserstack_apikey': '',
+	"login": {
+		"administrator": {
+			"username": "luke",
+			"password": "$$kumu09"
+		},
+		"basicuser": {
+			"username": "",
+			"password": ""
+		},
+		"intern": {
+			"username": "",
+			"password": ""
+		},
+		"readonly": {
+			"username": "",
+			"password": ""
+		}
+	}
+}
