a37e3a1dfc1b2adffa91efb7b27fe7e7f7110b66
luke <robotichead@gmail.com>
2020-08-28 18:06:06 +1000

    Fix to login page

---
+++ src/js/components/modules/wizards/NewBugWizard.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="modal fade" id="addBugsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h2><i data-feather="users"></i> Add Bugs Wizard</h2>
+                    <button type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                            id="addBugsCloseButton"
+                    >
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <strong>Adding Bugs</strong>
+                            <p class="text-instructions">
+
+                            </p>
+                        </div>
+                        <div class="col-md-8">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button"
+                            class="btn btn-primary"
+                            v-bind:diabled="bugModel == ''"
+                            v-on:click="bugModel"
+                    >
+                        Save changes
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NewBugWizard",
+        props: [
+            'destination',
+            'locationId',
+        ],
+        data() {
+            return {
+                bugModel: [],
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
