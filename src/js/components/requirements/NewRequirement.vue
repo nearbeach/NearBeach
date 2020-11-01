c122a6e4cd4221ee3e184bac924d1dc441af3851
robotichead <robotichead@gmail.com>
2020-11-01 23:32:17 +1100

    Trying to optimise TinyMce

--- src/js/components/requirements/NewRequirement.vue
+++ src/js/components/requirements/NewRequirement.vue
@@ -41,12 +41,12 @@
                        :init="{
                          height: 500,
                          menubar: false,
-                         skin:false,
-                         content_css:false,
                          toolbar: 'undo redo | formatselect | ' +
                           'bold italic backcolor | alignleft aligncenter ' +
                           'alignright alignjustify | bullist numlist outdent indent | ',
                        }"
+                       v-bind:content_css="false"
+                       v-bind:skin="false"
                        v-model="requirementScopeModel"
                     />
                 </div>
