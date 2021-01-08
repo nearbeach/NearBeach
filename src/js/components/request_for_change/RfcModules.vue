60c5078c95bb2e2152d6145c11759d62fc5a6784
robotichead <robotichead@gmail.com>
2021-01-08 18:23:05 +1100

    Implementation of RFC Modules

---
+++ src/js/components/request_for_change/RfcModules.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: "RfcModules"
+}
+</script>
+
+<style scoped>
+
+</style>
