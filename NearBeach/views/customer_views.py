8ff7e7015a3af13891f6b557e50fa64934e748fe
Luke <luke@nearbeach.org>
2021-03-06 16:51:05 +1100

    Fix to update customer removing organisation

--- NearBeach/views/customer_views.py
+++ NearBeach/views/customer_views.py
@@ -75,7 +75,7 @@ def customer_information_save(request,customer_id):
     customer_results.customer_first_name = form.cleaned_data['customer_first_name']
     customer_results.customer_last_name = form.cleaned_data['customer_last_name']
     customer_results.customer_email = form.cleaned_data['customer_email']
-    customer_results.organisation = form.cleaned_data['organisation']
+    # customer_results.organisation = form.cleaned_data['organisation'] # Does not need updating!
 
     # Save
     customer_results.save()
