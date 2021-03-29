352f62da83c576bd6782c0b21d3ed4b35d3041b9
robotichead <robotichead@gmail.com>
2021-03-30 00:05:12 +1100

    Implementing fixtures for testing

---
+++ NearBeach/tests/test_user_login.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+class LogInTest(TestCase):
+    fixtures = ['NearBeach_empty_setup']
+    def setUp(self):
+        self.credentials = {
+            'username': 'admin',
+            'password': 'Test1234$'}
+        # User.objects.create_user(**self.credentials)
+    def test_login(self):
+        # send login data
+        response = self.client.post(
+            reverse('login'),
+            self.credentials,
+            follow=True,
+        )
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
+        print("Tested User Loggin In")
