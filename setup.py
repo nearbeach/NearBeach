63dcbc0f92e2830f51e5606f6b40df469ac3d6d6
Luke <luke@nearbeach.org>
2021-04-19 21:00:26 +1000

    Update package version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="NearBeach",
-    version="0.27.8",
+    version="0.27.9",
     author="Luke Christopher Clarke",
     author_email="luke@nearbeach.org",
     description="NearBeach - an open source project management tool",
