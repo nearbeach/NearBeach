6a63ecc4c188ffba0b09e9866b0f22992ad378f2
Luke <luke@nearbeach.org>
2021-08-18 21:08:14 +1000

    BUG FIX - CircleCI Tests are utilising SQLite

--- tests/settings.py
+++ tests/settings.py
@@ -74,22 +74,22 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_db',
-        'USER': 'root',
-        'PASSWORD': 'rootpw',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'test_db',
+#         'USER': 'root',
+#         'PASSWORD': 'rootpw',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
 }
 
 # Password validation
