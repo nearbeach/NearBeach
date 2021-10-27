663ccbfa2562f9a2ffa41aa149aa9b78463102a8
Raul Rusu <raulrusu46@gmail.com>
2021-10-27 22:09:37 +0300

    removed the useless else condition.

--- setup.py
+++ setup.py
@@ -3,19 +3,23 @@ import codecs
 import os.path
 
 # Used to read the file
+
+
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, rel_path), 'r') as fp:
         return fp.read()
 
 # Used to extract out the __version__
+
+
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
         if line.startswith('__version__'):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
-    else:
         raise RuntimeError("Unable to find version string.")
+
 
 # Used to read the readme file
 with open("README.md", "r") as fh:
