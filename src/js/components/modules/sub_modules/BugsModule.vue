1f0a4f44432e5f3aa07e96e9fa7bc82939564433
Luke Christopher Clarke <luke@nearbeach.org>
2022-05-03 23:47:18 +1000

    Fix to BUG709 & BUG705

--- src/js/components/modules/sub_modules/BugsModule.vue
+++ src/js/components/modules/sub_modules/BugsModule.vue
@@ -173,6 +173,12 @@
             },
         },
         mounted() {
+            //If the location is inside the array - don't bother getting the data
+            var escape_array = [
+                'requirement_item',
+            ]
+            if (escape_array.indexOf(this.locationId) < 0) return;
+
             //Wait 200ms before getting data
             setTimeout(() => {
                 this.getBugList();
