18e36c575e4cb73807738fe9375f9d9d8d002043
Luke Christopher Clarke <luke@nearbeach.org>
2022-07-24 02:57:00 +1000

    Removing old functionality from login

--- NearBeach/static/NearBeach/tinymce/plugins/table/index.js
+++ NearBeach/static/NearBeach/tinymce/plugins/table/index.js
@@ -1,1 +1,7 @@
-require("./plugin.js");
+// Exports the "table" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/table')
+//   ES2015:
+//     import 'tinymce/plugins/table'
+require('./plugin.js');
