9f848f20a3e251b9d6ff12ae55a59eb8026e6e9e
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2022-10-07 15:50:58 +0800

    Remove unnecessary escape characters.

--- src/js/mixins/getTokenMixin.js
+++ src/js/mixins/getTokenMixin.js
@@ -2,7 +2,7 @@ export default {
     methods: {
         getToken: function(TokenName) {
             //Use regex to extract out the require token
-            const regEx = new RegExp(`${TokenName}\=([^;]+)`);
+            const regEx = new RegExp(`${TokenName}=([^;]+)`);
             const value = regEx.exec(document.cookie);
             return (value !== null) ? encodeURIComponent(value[1]) : null;
         }
