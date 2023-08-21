79eb05caae688abd57853243caa5b6a877be62df
Luke Clarke <lclarke@meshassist.com>
2023-08-21 23:05:59 +1000

    nearbeach-1056 Organisation default images are not showing in search

--- src/js/components/organisations/ListOrganisations.vue
+++ src/js/components/organisations/ListOrganisations.vue
@@ -1,42 +1,36 @@
 <template>
-	<div>
-		<div
-			v-for="organisation in organisationResults"
-			:key="organisation.id"
-			class="row"
-		>
-			<div class="organisation-details">
-				<img
-					v-bind:src="getProfilePicture(organisation)"
-					alt="Stakeholder Logo"
-					class="organisation-image"
-				/>
-				<div class="organisation-name">
-					<a
-						v-bind:href="`${rootUrl}organisation_information/${organisation.pk}/`"
-					>
-						{{ organisation.fields.organisation_name }}
-					</a>
-				</div>
-				<div class="organisation-link">
-					<Icon v-bind:icon="icons.linkOut"></Icon> Website:
-					<a
-						v-bind:href="organisation.fields.organisation_website"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						{{ organisation.fields.organisation_website }}
-					</a>
-				</div>
-				<div class="organisation-email">
-					<Icon v-bind:icon="icons.mailIcon"></Icon> Email:
-					<a
-						v-bind:href="`mailto:${organisation.fields.organisation_email}`"
-					>
-						{{ organisation.fields.organisation_email }}
-					</a>
-				</div>
-			</div>
+	<div v-for="organisation in organisationResults"
+		:key="organisation.id"
+		class="organisation-details">
+		<img
+			v-bind:src="getProfilePicture(organisation)"
+			alt="Stakeholder Logo"
+			class="organisation-image"
+		/>
+		<div class="organisation-name">
+			<a
+				v-bind:href="`${rootUrl}organisation_information/${organisation.pk}/`"
+			>
+				{{ organisation.fields.organisation_name }}
+			</a>
+		</div>
+		<div class="organisation-link">
+			<Icon v-bind:icon="icons.linkOut"></Icon> Website:
+			<a
+				v-bind:href="organisation.fields.organisation_website"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				{{ organisation.fields.organisation_website }}
+			</a>
+		</div>
+		<div class="organisation-email">
+			<Icon v-bind:icon="icons.mailIcon"></Icon> Email:
+			<a
+				v-bind:href="`mailto:${organisation.fields.organisation_email}`"
+			>
+				{{ organisation.fields.organisation_email }}
+			</a>
 		</div>
 	</div>
 </template>
@@ -75,7 +69,7 @@
 
 				//If customer profile is blank - return default picture
 				if (image === "" || image === null) {
-					return `${this.staticUrl}/NearBeach/images/placeholder/product_tour.svg`;
+					return `${this.staticUrl}NearBeach/images/placeholder/product_tour.svg`;
 				}
 
 				return `${this.rootUrl}private/${image}`;
