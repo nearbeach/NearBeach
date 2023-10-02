80d1719bfa4ef1e6c8e108dbe2279390cb745fbc
Mayank Gupta <67818466+Mayank-Creater@users.noreply.github.com>
2023-10-02 15:27:04 +0530

    Update BetweenDates.vue

--- src/js/components/dates/BetweenDates.vue
+++ src/js/components/dates/BetweenDates.vue
@@ -93,7 +93,7 @@ export default {
 		startDateModel: {
 			type: Number,
 			default: () => {
-				let temp_date = new Date();
+				const temp_date = new Date();
 				temp_date.setHours(9);
 				temp_date.setMinutes(0);
 				temp_date.setSeconds(0);
