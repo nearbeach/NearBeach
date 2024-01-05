7f2a60c4e94e14d97ecf1499378c91f67d4df55f
nearbeach <robotichead@gmail.com>
2024-01-05 20:33:01 +1100

    Bugfix - nearbeach-1207 RFC Validation needs to be refactored

---
+++ src/js/components/request_for_change/RfcWizard.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="rfc-wizard--loading-bar"
+		 v-bind:style="getStyle()"
+	></div>
+
+	<div class="rfc-wizard">
+		<div v-for="(tab, index) in tabContents"
+			 :key="tab"
+			 class="rfc-wizard--tab"
+		>
+			<div v-bind:class="getClasses(index)">
+				<span>{{ index + 1 }}</span>
+			</div>
+			<div class="rfc-wizard--tab-title">{{ tab }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "RfcWizard",
+	components: {},
+	props: {
+		currentTab: {
+			type: Number,
+			default: 0,
+		},
+	},
+	data: () => ({
+		tabContents: [
+			"Descript",
+			"Detail",
+			"Risk",
+			"Implement",
+			"Back-out",
+			"Test Plan"
+		],
+	}),
+	methods: {
+		getClasses(index) {
+			const classReturn = "rfc-wizard--tab-circle";
+
+			// If index is current tab - we highlight as current
+			if (parseInt(index) === this.currentTab) {
+				return classReturn + " current";
+			}
+
+			//If index is less than current tab, then we are completed
+			if (parseInt(index) < this.currentTab) {
+				return classReturn + " completed";
+			}
+
+			return classReturn;
+		},
+		getStyle() {
+			//Edge component will be 8.333% width
+			//Any length between the circles is 16.666%
+			let a = 8.333 + (16.333 * this.currentTab);
+
+			//Convert into string
+			return `width: ${a}%;`
+		}
+	},
+}
+</script>
