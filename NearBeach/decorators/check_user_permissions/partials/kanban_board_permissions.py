697c954cd910f7b53def474d68cd54c7ff73a020
nearbeach <robotichead@gmail.com>
2024-03-20 18:45:16 +1100

    Bugfix - nearbeach-1282 New Kanban board - checking board name gets 500 errors

--- NearBeach/decorators/check_user_permissions/partials/kanban_board_permissions.py
+++ NearBeach/decorators/check_user_permissions/partials/kanban_board_permissions.py
@@ -10,23 +10,24 @@ def kanban_board_permissions(request, kwargs):
         username=request.user,
     )
 
-    # Determine if there are any cross over with user groups and object_lookup groups
-    group_results = Group.objects.filter(
-        Q(
-            is_deleted=False,
-            # The object_lookup groups
-            group_id__in=ObjectAssignment.objects.filter(
+    if "kanban_board_id" in kwargs:
+        # Determine if there are any cross over with user groups and object_lookup groups
+        group_results = Group.objects.filter(
+            Q(
                 is_deleted=False,
-                kanban_board_id=kwargs["kanban_board_id"]
-            ).values("group_id"),
+                # The object_lookup groups
+                group_id__in=ObjectAssignment.objects.filter(
+                    is_deleted=False,
+                    kanban_board_id=kwargs["kanban_board_id"]
+                ).values("group_id"),
+            )
+            & Q(group_id__in=user_group_results.values("group_id"))
         )
-        & Q(group_id__in=user_group_results.values("group_id"))
-    )
 
-    # Check to make sure the user groups intersect
-    if len(group_results) == 0:
-        # There are no matching groups - i.e. the user does not have any permission
-        return False, 0
+        # Check to make sure the user groups intersect
+        if len(group_results) == 0:
+            # There are no matching groups - i.e. the user does not have any permission
+            return False, 0
 
     # Get the max permission value from user_group_results
     user_level = user_group_results.aggregate(
