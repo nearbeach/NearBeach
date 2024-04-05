aeae9c9dfa85af1a760e31af362dd9ae576a4b18
nearbeach <robotichead@gmail.com>
2024-04-05 22:21:42 +1100

    Bugfix - nearbeach-1316 Change Task delete - no modal to confirm deletion request

---
+++ src/js/components/change_task/modules/ConfirmChangeTaskDelete.vue
@@ -0,0 +1,88 @@
+<template>
+	<div
+		class="modal fade"
+		id="confirmChangeTaskDeleteModal"
+		tabindex="-1"
+		data-bs-backdrop="static"
+		data-bs-keyboard="false"
+		aria-labelledby="confirmChangeTaskDelete"
+		aria-hidden="true"
+	>
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5
+						class="modal-title"
+						id="confirmChangeTaskDelete"
+					>
+						Please confirm Change Task Deletion
+					</h5>
+					<!-- TASK INFORMATION -->
+					<button
+						type="button"
+						class="btn-close"
+						data-bs-dismiss="modal"
+						aria-label="Close"
+						id="confirmChangeTaskDeleteButton"
+					></button>
+				</div>
+				<div class="modal-body">
+					Are you sure you want to delete the Change Task from the Request for Change?
+				</div>
+				<div class="modal-footer">
+					<button
+						type="button"
+						class="btn btn-secondary"
+						v-on:click="closeModal"
+					>
+						No
+					</button>
+					<button
+						type="button"
+						class="btn btn-primary"
+						v-on:click="deleteChangeTask"
+					>
+						Yes
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import {mapGetters} from "vuex";
+
+export default {
+	name: "ConfirmChangeTaskDelete",
+	props: {
+		changeTaskResults: {
+			type: Array,
+			default: () => {
+				return [];
+			},
+		},
+	},
+	computed: {
+		...mapGetters({
+			destination: "getDestination",
+			locationId: "getLocationId",
+			rootUrl: "getRootUrl",
+		})
+	},
+	methods: {
+		deleteChangeTask() {
+			//Send the trigger
+			this.axios.post(
+				`${this.rootUrl}change_task_information/${this.changeTaskResults[0].pk}/delete/`
+			).then((response) => {
+				//If successful, go back
+				window.location.href = `${this.rootUrl}rfc_information/${this.changeTaskResults[0].fields.request_for_change}/`;
+			});
+		},
+		closeModal() {
+			document.getElementById("confirmChangeTaskDeleteButton").click();
+		}
+	},
+}
+</script>
