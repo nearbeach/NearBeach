256b32ff53d305d9f7c927403d7cb417b8f175d3
unknown <rashmit449@gmail.com>
2024-10-05 00:05:29 +0530

    Used const declarations for variables that are never reassigned

--- src/js/vuex/ganttChartVueX.js
+++ src/js/vuex/ganttChartVueX.js
@@ -1,80 +1,96 @@
 import { DateTime } from "luxon";
 
 export const moduleGantChart = {
-    state: () => ({
-        deltaDays: 0,
-        ganttChartData: [],
-        ganttStatusList: {
-            requirement_item_status: [],
-            project_status: [],
-            task_status: [],
-        },
-        endDateGantt: 0,
-        startDateGantt: 0,
-    }),
-    mutations: {
-        updateGanttChart(state, payload) {
-            // Defining int keys
-            const int_keys = ["deltaDays", "startDateGantt", "endDateGantt"];
+	state: () => ({
+		deltaDays: 0,
+		ganttChartData: [],
+		ganttStatusList: {
+			requirement_item_status: [],
+			project_status: [],
+			task_status: [],
+		},
+		endDateGantt: 0,
+		startDateGantt: 0,
+	}),
+	mutations: {
+		updateGanttChart(state, payload) {
+			// Defining int keys
+			const int_keys = ["deltaDays", "startDateGantt", "endDateGantt"];
 
-            // Loop through the payload and update the state
-            Object.keys(payload).forEach((key) => {
-                state[key] = int_keys.includes(key) ? parseInt(payload[key]) : payload[key];
-            });
-        },
-        updateGanttChartData(state, payload) {
-            state.ganttChartData = payload.ganttChartData;
-        },
-        updateGanttStatusList(state, payload) {
-            state.ganttStatusList = payload.ganttStatusList;
-        },
-    },
-    actions: {
-        initialiseGanttChartData: ({ state, commit }, payload) => {
-            // Calculate the delta days
-            let delta_days = Math.floor((payload.endDateGantt - payload.startDateGantt) / (1000 * 60 * 60 * 24));
-            if (delta_days === 0) delta_days = 1;
+			// Loop through the payload and update the state
+			Object.keys(payload).forEach((key) => {
+				state[key] = int_keys.includes(key)
+					? parseInt(payload[key])
+					: payload[key];
+			});
+		},
+		updateGanttChartData(state, payload) {
+			state.ganttChartData = payload.ganttChartData;
+		},
+		updateGanttStatusList(state, payload) {
+			state.ganttStatusList = payload.ganttStatusList;
+		},
+	},
+	actions: {
+		initialiseGanttChartData: ({ commit }, payload) => {
+			// Removed state
+			// Calculate the delta days
+			let delta_days = Math.floor(
+				(payload.endDateGantt - payload.startDateGantt) /
+					(1000 * 60 * 60 * 24)
+			);
+			if (delta_days === 0) delta_days = 1;
 
-            // Convert the Gantt chart data into the correct format
-            const gant_chart_data = payload.ganttChartData.map((row) => ({
-                ...row,
-                end_date: DateTime.fromISO(row.end_date).toMillis(),
-                start_date: DateTime.fromISO(row.start_date).toMillis(),
-            }));
+			// Convert the Gantt chart data into the correct format
+			const gant_chart_data = payload.ganttChartData.map((row) => ({
+				...row,
+				end_date: DateTime.fromISO(row.end_date).toMillis(),
+				start_date: DateTime.fromISO(row.start_date).toMillis(),
+			}));
 
-            // Commit the updated data
-            commit({
-                type: "updateGanttChart",
-                deltaDays: delta_days,
-                ganttChartData: gant_chart_data,
-                endDateGantt: payload.endDateGantt,
-                startDateGantt: payload.startDateGantt,
-            });
-        },
-        removeGanttChartSingleRow: ({ state, commit }, payload) => {
-            // Remove the row using the id and object type
-            const gantt_chart_data = state.ganttChartData.filter(row => (
-                payload.objectType !== row.object_type || parseInt(payload.objectId) !== parseInt(row.object_id)
-            ));
+			// Commit the updated data
+			commit({
+				type: "updateGanttChart",
+				deltaDays: delta_days,
+				ganttChartData: gant_chart_data,
+				endDateGantt: payload.endDateGantt,
+				startDateGantt: payload.startDateGantt,
+			});
+		},
+		removeGanttChartSingleRow: ({ commit }, payload) => {
+			// Removed state
+			// Remove the row using the id and object type
+			const gantt_chart_data = state.ganttChartData.filter(
+				(row) =>
+					payload.objectType !== row.object_type ||
+					parseInt(payload.objectId) !== parseInt(row.object_id)
+			);
 
-            // Commit the updated data
-            commit("updateGanttChartData", { ganttChartData: gantt_chart_data });
-        },
-        updateGanttChartSingleRow: ({ state, commit }, payload) => {
-            // Update the Gantt chart data for a single row
-            const gantt_chart_data = [...state.ganttChartData];
-            gantt_chart_data[payload.index] = payload.value;
+			// Commit the updated data
+			commit("updateGanttChartData", {
+				ganttChartData: gantt_chart_data,
+			});
+		},
+		updateGanttChartSingleRow: ({ commit }, payload) => {
+			// Removed state
+			// Update the Gantt chart data for a single row
+			const gantt_chart_data = [...state.ganttChartData];
+			gantt_chart_data[payload.index] = payload.value;
 
-            // Commit the updated data
-            commit("updateGanttChartData", { ganttChartData: gantt_chart_data });
-        },
-    },
-    getters: {
-        getDeltaDays: (state) => state.deltaDays,
-        getGanttChartData: (state) => state.ganttChartData,
-        getGanttChartDataSingleRow: (state) => (index) => state.ganttChartData[index],
-        getGanttStatusList: (state) => (object_type) => state.ganttStatusList[object_type],
-        getEndDateGantt: (state) => state.endDateGantt,
-        getStartDateGantt: (state) => state.startDateGantt,
-    },
+			// Commit the updated data
+			commit("updateGanttChartData", {
+				ganttChartData: gantt_chart_data,
+			});
+		},
+	},
+	getters: {
+		getDeltaDays: (state) => state.deltaDays,
+		getGanttChartData: (state) => state.ganttChartData,
+		getGanttChartDataSingleRow: (state) => (index) =>
+			state.ganttChartData[index],
+		getGanttStatusList: (state) => (object_type) =>
+			state.ganttStatusList[object_type],
+		getEndDateGantt: (state) => state.endDateGantt,
+		getStartDateGantt: (state) => state.startDateGantt,
+	},
 };
--- src/js/vuex/noteVueX.js
+++ src/js/vuex/noteVueX.js
@@ -1,66 +1,73 @@
 export const moduleNote = {
-    state: () => ({
-        noteDescription: "",
-        noteId: 0,
-        noteList: [],
-    }),
-    mutations: {
-        addNote(state, payload) {
-            state.noteList.push(payload.newNote);
-        },
-        initNoteList(state, payload) {
-            state.noteList = payload.noteList;
-        },
-        removeNote(state, payload) {
-            state.noteList = state.noteList.filter((row) => {
-                return parseInt(row.object_note_id) !== parseInt(payload.noteId);
-            });
-        },
-    },
-    actions: {
-        editSingleNote({ state }, payload) {
-            // Get the index of the array
-            const index = state.noteList.findIndex((row) => {
-                return parseInt(row.object_note_id) === parseInt(payload.noteId);
-            });
+	state: () => ({
+		noteDescription: "",
+		noteId: 0,
+		noteList: [],
+	}),
+	mutations: {
+		addNote(state, payload) {
+			state.noteList.push(payload.newNote);
+		},
+		initNoteList(state, payload) {
+			state.noteList = payload.noteList;
+		},
+		removeNote(state, payload) {
+			state.noteList = state.noteList.filter((row) => {
+				return (
+					parseInt(row.object_note_id) !== parseInt(payload.noteId)
+				);
+			});
+		},
+	},
+	actions: {
+		editSingleNote({ state }, payload) {
+			// Get the index of the array
+			const index = state.noteList.findIndex((row) => {
+				return (
+					parseInt(row.object_note_id) === parseInt(payload.noteId)
+				);
+			});
 
-            // If there are no indexes, escape
-            if (index < 0) return;
+			// If there are no indexes, escape
+			if (index < 0) return;
 
-            // Update the note description
-            let mutate_data = state.noteList;
-            mutate_data[index].object_note = payload.noteDescription;
+			// Update the note description
+			let mutate_data = state.noteList;
+			mutate_data[index].object_note = payload.noteDescription;
 
-            // Update state
-            state.noteList = mutate_data;
-        },
-        updateNoteId({ commit, state }, payload) {
-            // Update the note id
-            state.noteId = payload.noteId;
+			// Update state
+			state.noteList = mutate_data;
+		},
+		updateNoteId({ state }, payload) {
+			// Removed commit since it's not used
+			// Update the note id
+			state.noteId = payload.noteId;
 
-            const filtered_data = state.noteList.filter((row) => {
-                return parseInt(row.object_note_id) === parseInt(payload.noteId);
-            });
+			const filtered_data = state.noteList.filter((row) => {
+				return (
+					parseInt(row.object_note_id) === parseInt(payload.noteId)
+				);
+			});
 
-            if (filtered_data.length === 0) {
-                // Set description to empty and return
-                state.noteDescription = "";
-                return;
-            }
+			if (filtered_data.length === 0) {
+				// Set description to empty and return
+				state.noteDescription = "";
+				return;
+			}
 
-            // Update note description
-            state.noteDescription = filtered_data[0].object_note;
-        },
-    },
-    getters: {
-        getNoteList: (state) => {
-            return state.noteList;
-        },
-        getSingleNoteDescription: (state) => {
-            return state.noteDescription;
-        },
-        getSingleNoteId: (state) => {
-            return state.noteId;
-        },
-    },
+			// Update note description
+			state.noteDescription = filtered_data[0].object_note;
+		},
+	},
+	getters: {
+		getNoteList: (state) => {
+			return state.noteList;
+		},
+		getSingleNoteDescription: (state) => {
+			return state.noteDescription;
+		},
+		getSingleNoteId: (state) => {
+			return state.noteId;
+		},
+	},
 };
