0759b6420f58032603cf13ee4c1a5dfbea58d4fc
unknown <rashmit449@gmail.com>
2024-10-05 00:13:29 +0530

    Used const for variables that are never reassigned

--- src/js/mixins/reopenCardInformation.js
+++ src/js/mixins/reopenCardInformation.js
@@ -1,18 +1,17 @@
-import {Modal} from "bootstrap";
+import { Modal } from "bootstrap";
 
 export default {
     methods: {
         reopenCardInformation() {
-            //This method will determine if we need to reopen the card information modal.
+            // This method will determine if we need to reopen the card information modal.
             let cardModal = document.getElementById("cardInformationModal");
-            if (cardModal !== null)
-            {
+            if (cardModal !== null) {
                 cardModal = new Modal(cardModal);
                 cardModal.show();
             }
 
-            //Due to a bug with bootstrap, we have two divs with classes "modal-backdrop fade show"
-            //Lets remove one of them
+            // Due to a bug with bootstrap, we have two divs with classes "modal-backdrop fade show"
+            // Let's remove one of them
             setTimeout(() => {
                 const div_modal = document.getElementsByClassName("modal-backdrop fade show");
                 if (div_modal.length > 1) {
@@ -21,4 +20,4 @@ export default {
             }, 200);
         }
     }
-}
+};
--- src/js/vuex/usersettings/kanbanSettingsVueX.js
+++ src/js/vuex/usersettings/kanbanSettingsVueX.js
@@ -11,7 +11,7 @@ export const kanbanSettings = {
             state.levels = payload.levels;
         },
         updateCanDragCards(state, payload) {
-            //Updating canDragCards
+            // Updating canDragCards
             state.canDragCards = payload.canDragCards;
         },
         updateLevels(state, payload) {
@@ -19,45 +19,44 @@ export const kanbanSettings = {
         },
     },
     actions: {
-        async updateCanDragCards({dispatch, commit}, payload) {
-            //Wait for the data to finish updating
+        async updateCanDragCards({ dispatch, commit }, payload) {
+            // Wait for the data to finish updating
             await commit("updateCanDragCards", {
                 canDragCards: payload.canDragCards,
             });
 
-            //Update the backend
+            // Update the backend
             dispatch("updateSettingsBackend");
         },
-        async updateLevelCollapse({dispatch, commit, state}, payload) {
-            //Map in the changes
-            let current_levels = state.levels.map((row) => {
-                //If the payload.level_id != row.level_id, just return the data
+        async updateLevelCollapse({ dispatch, commit, state }, payload) {
+            // Map in the changes
+            const current_levels = state.levels.map((row) => {
+                // If the payload.level_id != row.level_id, just return the data
                 if (payload.level_id !== row.level_id) return row;
 
                 // Return the new structure with the flipped isCollapsed boolean
                 return {
                     level_id: payload.level_id,
                     is_collapsed: !row.is_collapsed,
-                }
-            })
+                };
+            });
 
-            //Commit the state
+            // Commit the state
             await commit("updateLevels", {
                 levels: current_levels,
             });
 
-            //ADD CODE TO UPDATE BACKEND :)
+            // ADD CODE TO UPDATE BACKEND :)
             dispatch("updateSettingsBackend");
         },
-        updateSettingsBackend({state}) {
-            //Grab the data and use axios to tell the backend to update the data
+        updateSettingsBackend({ state }) {
+            // Grab the data and use axios to tell the backend to update the data
             const data_to_send = new FormData();
             data_to_send.set("setting_type", "KANBAN_BOARD");
             data_to_send.set("setting_data", JSON.stringify({
                 canDragCards: state.canDragCards,
                 levels: state.levels,
             }));
-
 
             axios.post(
                 "/user_settings/update/",
@@ -74,16 +73,16 @@ export const kanbanSettings = {
             return state.canDragCards;
         },
         getLevelCollapseStatus: (state) => (level_id) => {
-            //Filter for the level
+            // Filter for the level
             const level_results = state.levels.filter(row => {
                 return row.level_id === level_id;
             });
 
-            //If array is empty - return default false
+            // If array is empty - return default false
             if (level_results.length === 0) return false;
 
-            //Return the first level results
+            // Return the first level results
             return level_results[0].is_collapsed;
         },
     },
-}
+};
