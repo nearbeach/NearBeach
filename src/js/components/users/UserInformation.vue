6486eab74c22ae9d1f6a49962c0228254d86b73c
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2024-10-16 14:26:22 +0100

    ["fields"] is better written in dot notation

--- src/js/components/users/UserInformation.vue
+++ src/js/components/users/UserInformation.vue
@@ -170,7 +170,7 @@ export default {
 	},
 	data() {
 		return {
-			emailModel: this.userResults[0]["fields"].email,
+			emailModel: this.userResults[0].fields.email,
 			isActiveModel: this.userResults[0].fields.is_active,
 			isSuperuserModel: this.userResults[0].fields.is_superuser,
 			firstNameModel: this.userResults[0].fields.first_name,
