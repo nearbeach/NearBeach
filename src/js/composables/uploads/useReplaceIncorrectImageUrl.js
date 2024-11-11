1b840679405cce048a4dd3850b95a0a983cc8a08
nearbeach <robotichead@gmail.com>
2024-11-11 20:50:32 +1100

    Code smell fixes - Found unnecessary escape characters JS-0097

--- src/js/composables/uploads/useReplaceIncorrectImageUrl.js
+++ src/js/composables/uploads/useReplaceIncorrectImageUrl.js
@@ -3,7 +3,7 @@ import { toValue } from "vue";
 export function useReplaceIncorrectImageUrl(input_string) {
     const value = toValue(input_string);
     const reg = new RegExp(
-        '/<img src="..\/private\/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}">'
+        '/<img src="../private/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}">'
     );
 
     //Using regex - we are finding the img src and removing the ../
