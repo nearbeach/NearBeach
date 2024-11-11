1ba61acfba59c2ce1614cbabe4ae17b0b2c241f8
nearbeach <robotichead@gmail.com>
2024-11-11 19:55:51 +1100

    Code Smell Fixes - Prefer the usage of regular expression literals over the RegExp constructor JS-0115

--- src/js/composables/uploads/useReplaceIncorrectImageUrl.js
+++ src/js/composables/uploads/useReplaceIncorrectImageUrl.js
@@ -5,7 +5,7 @@ export function useReplaceIncorrectImageUrl(input_string) {
 
     //Using regex - we are finding the img src and removing the ../
     return value.replace(
-        new RegExp("<img src=\"..\\/private\\/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\">"),
+        new RegExp(`<img src="..\\/private\\/[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}">`),
         (match) => {
             return match.replace("../", "/");
         }
