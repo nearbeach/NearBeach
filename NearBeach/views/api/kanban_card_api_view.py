5df33c930e0332932c0b6dee1f53bfd1ee9b460a
Luke Clarke <lclarke@meshassist.com>
2025-05-25 00:49:01 +1000

    Save night work - added in some crud for the kanban card id. Need to still implement UPDATE and CREATE

--- NearBeach/views/api/kanban_card_api_view.py
+++ NearBeach/views/api/kanban_card_api_view.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from NearBeach.models import (
     KanbanBoard,
     KanbanCard,
+    KanbanColumn,
+    KanbanLevel,
 )
 from NearBeach.serializers.kanban_card_serializer import KanbanCardSerializer
 from NearBeach.serializers.tag_serializer import TagSerializer
@@ -14,21 +16,38 @@ class KanbanCardViewSet(viewsets.ModelViewSet):
     queryset = KanbanCard.objects.filter(is_deleted=False)
     serializer_class = KanbanCardSerializer
 
+    @check_user_api_permissions(min_permission_level=2)
     def create(self, request, *args, **kwargs):
         return status.HTTP_500_INTERNAL_SERVER_ERROR
 
+    @check_user_api_permissions(min_permission_level=2)
     def destroy(self, request, *args, **kwargs):
-        return status.HTTP_500_INTERNAL_SERVER_ERROR
+        kanban_card = self.get_object()
+        kanban_card.is_deleted = True
+        kanban_card.change_user = request.user
+        kanban_card.save()
+
+        return Response(
+            data="kanban card deleted",
+            status=status.HTTP_204_NO_CONTENT,
+        )
 
     @check_user_api_permissions(min_permission_level=1)
     def list(self, request, *args, **kwargs):
-        page_size = int(request.query_params.get("page_size", 100))
-        page_size = page_size if page_size <= 1000 else 1000
-        page = int(request.query_params.get("page", 1))
-
         kanban_card_results = KanbanCard.objects.filter(
+            is_archived=False,
             is_deleted=False,
             kanban_board_id=kwargs["kanban_board_id"],
+            # Don't send cards that are in deleted Columns
+            kanban_column_id__in=KanbanColumn.objects.filter(
+                is_deleted=False,
+                kanban_board_id=kwargs["kanban_board_id"],
+            ),
+            # Don't send cards that are in deleted Levels
+            kanban_level_id__in=KanbanLevel.objects.filter(
+                is_deleted=False,
+                kanban_board_id=kwargs["kanban_board_id"],
+            ),
         )
 
         serializer = KanbanCardSerializer(kanban_card_results, many=True)
@@ -38,5 +57,19 @@ class KanbanCardViewSet(viewsets.ModelViewSet):
             status=status.HTTP_200_OK,
         )
 
-    def retrieve(self, request, *args, **kwargs):
-        return status.HTTP_500_INTERNAL_SERVER_ERROR
+    @check_user_api_permissions(min_permission_level=1)
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        queryset = KanbanCard.objects.filter(
+            is_deleted=False,
+        )
+        kanban_card_results = get_object_or_404(
+            queryset,
+            pk=pk,
+        )
+
+        serializer = KanbanCardSerializer(kanban_card_results)
+
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK,
+        )
